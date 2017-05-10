f3ff3d4413d9da6488fe44227ac638e4a23302e8
lmc297 <lmc297@cornell.edu>
2017-05-10 18:02:15 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -4,7 +4,6 @@ class Btyper < Formula
   url "https://github.com/lmc297/BTyper/btyper-0.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   version "0.0.0"
-  head "https://github.com/lmc297/BTyper.git"
   
   depends_on :python
   depends_on "blast"
