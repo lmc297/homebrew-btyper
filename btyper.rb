a3fbd700d96d2e84ea25dfe19999af26d0d615b9
lmc297 <lmc297@cornell.edu>
2017-05-10 14:07:00 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -1,19 +1,19 @@
 class Btyper < Formula
- desc "A command line tool for classifying Bacillus cereus group isolates from nucleotide sequencing data"
- homepage "https://github.com/lmc297/homebrew-btyper"
- url "https://github.com/lmc297/homebrew-btyper/tree/master/archive/btyper-0.0.0.tar.gz"
- sha256 ""
- version "0.0.0"
- depends_on :python
- depends_on "blast"
- depends_on "spades"
- depends_on "sratoolkit"
- #resource "biopython" do
- #	url "http://biopython.org/wiki/Download/biopython-1.69.tar.gz"
- #	sha1
- #end
- bottle :unneeded
- def install
-  bin.install "btyper"
-  end
+  desc "A command line tool for classifying Bacillus cereus group isolates from nucleotide sequencing data"
+  homepage "https://github.com/lmc297/homebrew-btyper"
+  url "https://github.com/lmc297/homebrew-btyper/tree/master/archive/btyper-0.0.0.tar.gz"
+  sha256 ""
+  version "0.0.0"
+  depends_on :python
+  depends_on "blast"
+  depends_on "spades"
+  depends_on "sratoolkit"
+  # resource "biopython" do
+  #	url "http://biopython.org/wiki/Download/biopython-1.69.tar.gz"
+  #	sha1
+  #end
+  bottle :unneeded
+    def install
+    bin.install "btyper"
+    end
 end
