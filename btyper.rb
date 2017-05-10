28a53cfe487efa28b95275a75214484808fc0571
lmc297 <lmc297@cornell.edu>
2017-05-10 14:12:34 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -2,7 +2,7 @@ class Btyper < Formula
   desc "A command line tool for classifying Bacillus cereus group isolates from nucleotide sequencing data"
   homepage "https://github.com/lmc297/homebrew-btyper"
   url "https://github.com/lmc297/homebrew-btyper/tree/master/archive/btyper-0.0.0.tar.gz"
-  sha256 ""
+  sha256 "0a70f1c85fa14cd910df0b5fc109ca9515d3eb45077907f0e5c012f80324186d"
   version "0.0.0"
   depends_on :python
   depends_on "blast"
