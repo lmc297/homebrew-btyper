3f4672c1534b4a4b5f011ab0ee981f1451626e7a
lmc297 <lmc297@cornell.edu>
2017-05-12 17:38:03 -0400

    Update btyper-biopython.rb

--- btyper-biopython.rb
+++ btyper-biopython.rb
@@ -1,18 +1,13 @@
 class BtyperBiopython < Formula
   desc "A command line tool for classifying Bacillus cereus group isolates from nucleotide sequencing data"
   homepage "https://github.com/lmc297/BTyper/"
-  url "https://github.com/lmc297/BTyper/raw/master/archive/btyper-0.0.0.tar.gz"
-  sha256 "7320e5bb3743d2a36df7586358fb3f436cd9a049e04540ce9a9bcb7794971752"
-  version "0.0.0"
-  
+  url "http://biopython.org/DIST/biopython-1.69.tar.gz"
+  sha256 "169ffa90c3d3ec5678c7a5c99501c0cfeb54c40ca51a619ce6cee5026d3403eb"
+ 
   depends_on :python
   depends_on "numpy"
   
-  resource "biopython" do
-    url "http://biopython.org/DIST/biopython-1.69.tar.gz"
-    sha256 "169ffa90c3d3ec5678c7a5c99501c0cfeb54c40ca51a619ce6cee5026d3403eb"
-  end
- 
+  
   bottle :unneeded 
    
   def install
