3e089272f2fda069289e58df330aebe04ad902b6
lmc297 <lmc297@cornell.edu>
2017-05-12 13:29:14 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -10,6 +10,14 @@ class Btyper < Formula
   depends_on "spades"
   depends_on "sratoolkit"
   depends_on "numpy"
+  
+  resource "biopython" do
+    url "http://biopython.org/DIST/biopython-1.69.tar.gz"
+    sha256 "169ffa90c3d3ec5678c7a5c99501c0cfeb54c40ca51a619ce6cee5026d3403eb"
+  end
+  resource("biopython").stage do
+    system "python2", *Language::Python.setup_install_args(libexec)
+  end
     
   bottle :unneeded
    
