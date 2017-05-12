beb9b552d5255a51887d7b9dfd8f82bd32805291
lmc297 <lmc297@cornell.edu>
2017-05-12 13:09:56 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -18,6 +18,6 @@ class Btyper < Formula
   bottle :unneeded
    
   def install
-    bin.install "btyper", "seq_virulence_db", "seq_mlst_db", "seq_panC_db", "seq_rpoB_db", "seq_16s_db", "biopython"  
+    bin.install "btyper", "seq_virulence_db", "seq_mlst_db", "seq_panC_db", "seq_rpoB_db", "seq_16s_db"
   end
 end
