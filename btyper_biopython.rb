ce2d95bcdc646c6bc7920519b812deff35dc16c2
lmc297 <lmc297@cornell.edu>
2017-05-12 16:56:40 -0400

    Create btyper_biopython.rb

---
+++ btyper_biopython.rb
@@ -0,0 +1,30 @@
+class Biotyper < Formula
+  desc "A command line tool for classifying Bacillus cereus group isolates from nucleotide sequencing data"
+  homepage "https://github.com/lmc297/BTyper/"
+  url "https://github.com/lmc297/BTyper/raw/master/archive/btyper-0.0.0.tar.gz"
+  sha256 "7320e5bb3743d2a36df7586358fb3f436cd9a049e04540ce9a9bcb7794971752"
+  version "0.0.0"
+  
+  depends_on :python
+  depends_on "numpy"
+  
+  resource "biopython" do
+    url "http://biopython.org/DIST/biopython-1.69.tar.gz"
+    sha256 "169ffa90c3d3ec5678c7a5c99501c0cfeb54c40ca51a619ce6cee5026d3403eb"
+  end
+ 
+  bottle :unneeded 
+   
+  def install
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
+    %w[biopython].each do |r|
+      resource(r).stage do
+        system "python", *Language::Python.setup_install_args(libexec/"vendor")
+      end
+    end
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    system "python", *Language::Python.setup_install_args(libexec)
+    bin.install Dir[libexec/"bin/*"]
+    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  end
+end
