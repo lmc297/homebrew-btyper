586843e93de247610885d2a28caef2d3636f822c
lmc297 <lmc297@cornell.edu>
2017-05-12 13:43:19 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -20,8 +20,9 @@ class Btyper < Formula
    
   def install
     bin.install "btyper", "seq_virulence_db", "seq_mlst_db", "seq_panC_db", "seq_rpoB_db", "seq_16s_db"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
     resource("biopython").stage do
-      system "python2", *Language::Python.setup_install_args(libexec)
+      system "python", *Language::Python.setup_install_args(libexec/"vendor")
     end
   end
 end
