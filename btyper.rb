2bc8133f9c73447c29350b5dd725e558658afc83
lmc297 <lmc297@cornell.edu>
2017-05-13 00:16:06 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -10,9 +10,15 @@ class Btyper < Formula
   depends_on "spades"
   depends_on "sratoolkit"
   
+  resource "biopython" do
+    url "http://biopython.org/DIST/biopython-1.69.tar.gz"
+    sha256 "169ffa90c3d3ec5678c7a5c99501c0cfeb54c40ca51a619ce6cee5026d3403eb"
+  end
+  
   bottle :unneeded 
    
   def install
     bin.install "btyper", "seq_virulence_db", "seq_mlst_db", "seq_panC_db", "seq_rpoB_db", "seq_16s_db"
+    resource("biopython").stage { bin.install "/Library/Python/2.7/site-packages" }
   end
 end
