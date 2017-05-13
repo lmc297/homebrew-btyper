c931d4f0c549720f80c61bebe4c56e30e35a7592
lmc297 <lmc297@cornell.edu>
2017-05-13 01:18:32 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -35,7 +35,7 @@ class Btyper < Formula
     libexec.install Dir["*"]
     puts "done with libexecinstall"
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
-    bin.env_script_all_files(libexec/"lib/python2.7/site-packages", :PYTHONPATH => ENV["PYTHONPATH"])
+    bin.env_script_all_files(libexec/"lib/python2.7/site-packages/Bio", :PYTHONPATH => ENV["PYTHONPATH"])
     bin.env_script_all_files(libexec/"lib64/python2.7/site-packages", :PYTHONPATH => ENV["PYTHONPATH"])
 
     puts "done with errthing"
