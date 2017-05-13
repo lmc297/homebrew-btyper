0e5f08af1d1c3946ba34567fd36c7f7d2c52183e
lmc297 <lmc297@cornell.edu>
2017-05-13 01:26:42 -0400

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -35,7 +35,6 @@ class Btyper < Formula
     puts "done with libexecinstall"
     bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
     bin.env_script_all_files(libexec/"lib/python2.7/site-packages/Bio", :PYTHONPATH => ENV["PYTHONPATH"])
-    bin.env_script_all_files(libexec/"lib64/python2.7/site-packages", :PYTHONPATH => ENV["PYTHONPATH"])
 
     puts "done with errthing"
     
