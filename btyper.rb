8de40a564b49baf961818455eb40832fe9f306c7
Laura Carroll <lmc297@cornell.edu>
2018-03-21 21:26:01 +0100

    Update btyper.rb

--- btyper.rb
+++ btyper.rb
@@ -3,9 +3,9 @@ class Btyper < Formula
   homepage "https://github.com/lmc297/BTyper/"
   url "https://github.com/lmc297/BTyper/raw/master/archive/btyper-2.2.0.tar.gz"
   sha256 "81fc40f7d15cfa2844884c4945cae4044721e8e7420e85997d5d8a5bdd63c63b"
-  version "2.1.0"
+  version "2.2.0"
   
-  depends_on "python"
+  #depends_on "python"
   depends_on "blast"
   depends_on "spades"
   depends_on "sratoolkit"
